4ff9292385deb73d712f40f57e1e908998c8dfe4
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-11-24 09:41:09 +0100

    Dit hoort bij opdracht: F1.6.01.L2 - I'm nutty and I know it! - Bag of M&M's als een Dictionary

--- nutty-candy.py
+++ nutty-candy.py
@@ -2,13 +2,29 @@ import random
 
 colorList = ['oranje', 'blauw', 'groen', 'bruin']
 
-def createBag(colors):
-    chosencolors = []
+dictionarylist = {
+
+        'oranje' : 0,
+        'blauw' : 0,
+        'groen' : 0,
+        'bruin' : 0
+        }
+
+def createBaglist(colors):
+    chosencolorslist = []
     for x in range(colors):
         pickColor = random.randrange(0,4)
-        chosencolors.append(colorList[pickColor])
-    return chosencolors
+        chosencolorslist.append(colorList[pickColor])
+    return chosencolorslist
+
+
+def createBagdictionary(colors):
+    for y in range(colors):
+        pickColor = random.choice(colorList)
+        dictionarylist[pickColor] = dictionarylist[pickColor] +1
+    return dictionarylist
 
 ammoutcolors = int(input("Hoeveel kleuren wilt u aan de zak toevoegen? "))
-showColors = createBag(ammoutcolors)
-print(showColors)
+# showColors = createBaglist(ammoutcolors)
+showColorsdictionary = createBagdictionary(ammoutcolors)
+print(showColorsdictionary)
