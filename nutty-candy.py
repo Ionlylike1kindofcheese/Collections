b5104d50afd1b63adafd059fecb43e37caa4c336
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2021-11-26 10:04:52 +0100

    Dit hoort bij opdracht: F1.6.01.L3 - Sort my bags! (Lukte niet helemaal op de manier zoals ik het in gedachte had dus moest improviseren)

--- nutty-candy.py
+++ nutty-candy.py
@@ -2,13 +2,7 @@ import random
 
 colorList = ['oranje', 'blauw', 'groen', 'bruin']
 
-dictionarylist = {
-
-        'oranje' : 0,
-        'blauw' : 0,
-        'groen' : 0,
-        'bruin' : 0
-        }
+dictionarylist = {'oranje' : 0, 'blauw' : 0, 'groen' : 0, 'bruin' : 0}
 
 def createBaglist(colors):
     chosencolorslist = []
@@ -24,7 +18,52 @@ def createBagdictionary(colors):
         dictionarylist[pickColor] = dictionarylist[pickColor] +1
     return dictionarylist
 
-ammoutcolors = int(input("Hoeveel kleuren wilt u aan de zak toevoegen? "))
-# showColors = createBaglist(ammoutcolors)
-showColorsdictionary = createBagdictionary(ammoutcolors)
-print(showColorsdictionary)
+
+def sortBags(readyforsort, ammoutmnm):
+    ammoutorange = 0
+    ammoutblue = 0
+    ammoutgreen = 0
+    ammoutbrown = 0
+    currentposition = 0
+    sorting = False
+    while sorting == False:
+        while currentposition < ammoutmnm:
+            colorcheck = False
+            while colorcheck == False:
+                if readyforsort[currentposition] == "oranje":
+                    ammoutorange += 1
+                    colorcheck = True
+                elif readyforsort[currentposition] == "blauw":
+                    ammoutblue += 1
+                    colorcheck = True
+                elif readyforsort[currentposition] == "groen":
+                    ammoutgreen += 1
+                    colorcheck = True
+                elif readyforsort[currentposition] == "bruin":
+                    ammoutbrown += 1
+                    colorcheck = True
+                else:
+                    None
+            currentposition += 1
+        sorting = True
+    listcreation = False
+    sortedcolorsammout = [ammoutorange, ammoutblue, ammoutgreen, ammoutbrown]
+    sortedcolors = []
+    currentsortedcolorposition = 0
+    while listcreation == False:
+        for b in range(sortedcolorsammout[currentsortedcolorposition]):
+            sortedcolors.append(colorList[currentsortedcolorposition])
+        currentsortedcolorposition += 1
+        if currentsortedcolorposition >= 4:
+            listcreation = True
+    dictionarylist = {'oranje' : ammoutorange, 'blauw' : ammoutblue, 'groen' : ammoutgreen, 'bruin' : ammoutbrown}
+    print(dictionarylist)
+    return sortedcolors
+        
+                            
+
+ammoutcolors = int(input("Hoeveel M&Ms wilt u aan de zak toevoegen? "))
+mnmcount = ammoutcolors
+chosencolorsoutside = createBaglist(ammoutcolors) 
+showColorslist = sortBags(chosencolorsoutside, mnmcount)
+print(showColorslist)
